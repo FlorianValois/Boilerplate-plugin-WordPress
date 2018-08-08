ad33bb8a7c9dc1b2aa2cf20f3077b884f1206ebd
FlorianValois <contact@florian-valois.com>
2018-08-08 15:19:12 +0200

    version v0.0.2

--- boilerplate-plugin.php
+++ boilerplate-plugin.php
@@ -7,7 +7,7 @@ Author: Florian Valois
 Author URI: https://florian-valois.com/
 Text Domain: boilerplate-plugin
 Domain Path: /languages/
-Version: 0.1
+Version: 0.0.2
 */
 
 if( ! class_exists( 'Smashing_Updater' ) ){
