b0c66b4aac045eee38640a8d264de56827eb2a74
FlorianValois <contact@florian-valois.com>
2018-08-09 12:26:43 +0200

    modif scss + version number

--- boilerplate-plugin-wordpress.php
+++ boilerplate-plugin-wordpress.php
@@ -8,7 +8,7 @@ Author: Florian Valois
 Author URI: https://florian-valois.com/
 Text Domain: boilerplate-plugin-wordpress
 Domain Path: /languages/
-Version: 0.1.5
+Version: 0.1.6
 */
 
 require 'plugin-update-checker/plugin-update-checker.php';
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate link: http://florian-valois.com/
 Requires at least: 4.0
 Tested up to: 4.9.8
 Requires PHP: 5.6
-Stable tag: 0.1.5
+Stable tag: 0.1.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
