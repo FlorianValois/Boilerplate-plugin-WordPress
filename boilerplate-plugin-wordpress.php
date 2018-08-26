1cfced683f40a1b1b08f5d6dc7d528d5ac385e80
Florian Valois <contact@florian-valois.com>
2018-08-26 14:38:05 +0200

    add comment to function and optimize call of this function

--- boilerplate-plugin-wordpress.php
+++ boilerplate-plugin-wordpress.php
@@ -46,16 +46,3 @@ function your_function_name(){
 <div id="scrollTop" style="font-family: Font Awesome 5 Free;"><i class="fas fa-caret-up"></i></div>
 <?php
 };
-
-
-add_action( 'init', 'run_this_code');
-function run_this_code() {
-  ob_start();
-  $data = get_option('boilerplate_plugin');
-  include(BPW_PLUGIN_DIR.'/assets/style.php');
-  $content = ob_get_contents();
-  ob_end_clean();
-  $f = fopen(BPW_PLUGIN_DIR.'/assets/style-php.css', 'w');
-  fwrite($f, $content);
-  fclose($f);
-}
--- includes/update-data.inc.php
+++ includes/update-data.inc.php
@@ -18,11 +18,26 @@ function ajax_form_update_options(){
   $option_name = 'boilerplate_plugin' ;
       
   if($_POST['data']){
+    
+    // Sauvegarde des data
     echo json_encode(array(
       'update' => update_option( $option_name, $params )
     ));
+    
+    // Génération du fichier CSS des options
+    ob_start();
+      $data = get_option('boilerplate_plugin');
+      include(BPW_PLUGIN_DIR.'/assets/style.php');
+      $content = ob_get_contents();
+    ob_end_clean();
+    $f = fopen(BPW_PLUGIN_DIR.'/assets/style-php.css', 'w');
+    fwrite($f, $content);
+    fclose($f);
+    
   }
   else{
+    
+    // Suppression des data
     echo json_encode(array(
       'delete' => delete_option( $option_name )
     ));
